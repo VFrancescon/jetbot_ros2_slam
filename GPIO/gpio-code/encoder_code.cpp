24d5e6c385638b7444d1b9a2fc89da1f267faf45
VFrancescon <vittorio.francescon@gmail.com>
2021-03-16 10:40:44 +0000

    Updated testing code for demonstration

--- GPIO/gpio-code/encoder_code.cpp
+++ GPIO/gpio-code/encoder_code.cpp
@@ -2,7 +2,10 @@
 #include <chrono>
 #include <thread>
 #include <csignal>
+#include <math.h>
 #include <JetsonGPIO.h>
+
+#define _USE_MATH_DEFINES
 
 using namespace std;
 bool end_this_program = false;
@@ -30,19 +33,28 @@ int main(){
     int curr_value_B, past_value_B = GPIO::LOW, tick_no_B = 0;
 
     while(!end_this_program){
-        //delay(1);
+        //delay(1); // Toggle the output every second
         curr_value_A = GPIO::input(input_pin_A);
         curr_value_B = GPIO::input(input_pin_B);
-        // Toggle the output every second
         if(curr_value_A != past_value_A){
             tick_no_A++;
-            cout << "Reading " << curr_value_A << " from pin ";
-            cout << input_pin_A << " tick number " << tick_no_A << "\n";
+            if(tick_no_A % 20 == 0){
+                cout << "Reading " << curr_value_A << " from pin ";
+                cout << input_pin_A << " tick number " << tick_no_A << "\n";
+            }
         }
         if(curr_value_B != past_value_B){
             tick_no_B++;
-            cout << "Reading " << curr_value_B << " from pin ";
-            cout << input_pin_B << " tick number " << tick_no_B << "\n";
+            if(tick_no_B % 20 == 0) {
+                cout << "Reading " << curr_value_B << " from pin ";
+                cout << input_pin_B << " tick number " << tick_no_B << "\n";
+            }
+        }
+        if(tick_no_A % 10 == 0){
+            cout << "Distance covered on motor A " << double(tick_no_A) * (3*M_PI / 10);
+        }
+        if(tick_no_B % 10 == 0){
+            cout << "Distance covered on motor B " << double(tick_no_B) * (3*M_PI / 10);
         }
         past_value_A = curr_value_A;
         past_value_B = curr_value_B;
--- test_code.py
+++ test_code.py
@@ -2,9 +2,8 @@ from jetbot import Robot
 import time
 
 robot = Robot()
-print("Running test now. Setting speed to 0.3")
-robot.left(speed=0.3)
-time.sleep(0.5)
+print("Running test now. Setting speed to 0.5")
+robot.foward(speed=0.5)
+time.sleep(5)
 robot.stop()
-print("And done")
-quit()
+
